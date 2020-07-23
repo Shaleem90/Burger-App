1c43023817277ced2f09b0c435acff120ff8462a
Shaleem Malik <shaleemmalik6@gmail.com>
2020-07-23 16:04:02 -0400

    made changes to connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,45 +1,18 @@
-// // Set up MySQL connection.
-// const mysql = require("mysql");
 
-// const password = process.env.PASSWORD;
+ const mysql = require("mysql");
 
-// if(process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: password,
-//     database: "burgers_db"
-//   })
-// }
-
-// // Make connection.
-// connection.connect();
-
-// // Export connection for our ORM to use.
-// module.exports = connection;
-
-// Set up MySQL connection.
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Shajee123!",
-  database: "burgers_db"
-});
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
-module.exports = connection;
+ if (process.env.JAWSDB_URL) {
+     connection = mysql.createConnection(process.env.JAWSDB_URL);
+ } else {
+ 
+     connection = mysql.createConnection({
+         host: "localhost",
+         port: 3306,
+         user: "root",
+         password: "Shajee123!",
+         database: "burgers_db"
+     })
+ };
+ connection.connect();
+ 
+ module.exports = connection;
