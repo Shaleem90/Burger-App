de710ba28ac140644bffc0f15e41f98a978426ae
Shaleem Malik <shaleemmalik6@gmail.com>
2020-07-23 15:21:38 -0400

    made changes to orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,7 @@
 // Import MySQL connection.
 const connection = require("../config/connection.js");
 
-//Function to create dynamically as many ? as we need depending on the values we want to insert at create.
+
 function printQuestionMarks(num) {
     let arr = [];
     for (let i = 0; i < num; i++) {
@@ -10,14 +10,14 @@ function printQuestionMarks(num) {
     return arr.toString();
   }
   
-  // Helper function to convert object key/value pairs to SQL syntax
+  //Covert to SQL syntax
   function objToSql(ob) {
     let arr = [];
   
     // loop through the keys and push the key/value as a string int arr
     for (let key in ob) {
       let value = ob[key];
-      // check to skip hidden properties
+      
       if (Object.hasOwnProperty.call(ob, key)) {
         // if string with spaces, add quotations 
         if (typeof value === "string" && value.indexOf(" ") >= 0) {
@@ -27,7 +27,7 @@ function printQuestionMarks(num) {
       }
     }
   
-    // translate array of strings to a single comma-separated string
+   
     return arr.toString();
   }
   
